8f75f549ff1639cf158e8ed234f1faf170c7c48d
shunmathi <shunu17796@gmail.com>
2018-08-20 00:11:14 +0530

    directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,49 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
-}
+export class AppComponent  {
+  //ch-66 ngSwitch
+  // viewmode='map';
+  //ch-67 ngFor
+  // courses=[
+  //   { id:'1',name:'course1'},
+  //   { id:'2',name:'course2'},
+  //   { id:'3',name:'course3'},
+    
+  // ];
+
+  //ch-68 ngFor and chng detection
+  // courses=[
+  //     { id:'1',name:'course1'},
+  //     { id:'2',name:'course2'},
+  //    { id:'3',name:'course3'},
+  //    ];
+  //    onAdd()
+  //    {
+  //      this.courses.push({id:'4',name:"course4"})
+  //      this.courses.push({id:'5',name:'course5'})
+  //    }
+  //    onRemove(course)
+  //    {
+  //      let index=this.courses.indexOf(course);
+  //      this.courses.splice(index,2);
+  //    }
+  //    onChange(course)
+  //    {course.name='updated';
+  //    }
+
+  //ch-69 ngfor trackBy-optimises both lines of code and memory
+  // courses;
+  // onload(){
+  //   this.courses=[
+  //         { id:'1',name:'course1'},
+  //         { id:'2',name:'course2'},
+  //        { id:'3',name:'course3'},
+  //        ];
+  // }
+  // trackreference(index,course){
+  //   return course? course.id : undefined;//checks if course is present,if yes returns its id
+  // }
+   // ch -72 ngStyle  
+   //canSave=false;
+    }
---
+++ src/app/input-format.directive.ts
@@ -0,0 +1,18 @@
+import { Directive, ElementRef, Input, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appInputFormat]'
+})
+export class InputFormatDirective {
+@Input('appInputFormat') format;
+  constructor(private el:ElementRef) { }
+  @HostListener('blur') onblur(){
+    let value:string=this.el.nativeElement.value;
+    if(this.format=='lowercase')
+    this.el.nativeElement.value=value.toLowerCase();
+    else
+    this.el.nativeElement.value=value.toUpperCase();
+    
+  }
+
+}
